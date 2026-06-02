aab611bc768a8ffc3eeca5d1cfe8f4daaf874aa2
Brandon T <131196207+brandiob@users.noreply.github.com>
2026-06-02 00:53:16 -0400

    fix

--- src/project.v
+++ src/project.v
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`default_nettype none`
+`default_nettype none
 
 module tt_um_uwasic_onboarding_brandon_thomas (
     input  wire [7:0] ui_in,    // Dedicated inputs
